dc7ca77e986cb37a837bbd40d1142eac503f60cd
Filipe Esperandio <filipesperandio@gmail.com>
2017-09-13 18:18:45 -0300

    Skip unecessary URL query string

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-URL=$(curl -s -i https://sourceforge.net/projects/checkstyle/files/latest/download | grep Location | awk '{print $2}')
-ESCAPED_URL=$(echo $URL | sed 's/\&/\\&/g')
-sed -i -E "s#URL=.*#URL='${ESCAPED_URL}'#" bin/install-checkstyle.sh
+URL=$(curl -s -i https://sourceforge.net/projects/checkstyle/files/latest/download | grep Location | awk '{print $2}'| sed 's/\?.*$//')
+sed -i -E "s#URL=.*#URL='${URL}'#" bin/install-checkstyle.sh
 
-echo $ESCAPED_URL
+echo $URL
