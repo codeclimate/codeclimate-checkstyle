a43a9e48bd775bdddcb8e9a55d8a49d20b520c83
Abby Edwards <abby.lina.edwards@gmail.com>
2019-11-11 13:03:08 -0800

    Upgrade checkstyle to 8.25 and fix upgrade script (#22)
    * Fix upgrade script to reliably find latest URL
    
    * Upgrade checkstyle from 8.2 to 8.25
    
    * Specify LineLength module as top-level module in default config
    
    * Remove unsupported property from JavadocMethod module config

--- bin/install-checkstyle.sh
+++ bin/install-checkstyle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # use `make upgrade` to update this URL to the latest version
-URL='https://downloads.sourceforge.net/project/checkstyle/checkstyle/8.2/checkstyle-8.2-all.jar'
+URL='https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.25/checkstyle-8.25-all.jar'
 
 wget -O /usr/local/bin/checkstyle.jar $URL
--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-URL=$(curl -s -i https://sourceforge.net/projects/checkstyle/files/latest/download | grep Location | awk '{print $2}'| sed 's/\?.*$//')
+URL=$(curl -s https://api.github.com/repos/checkstyle/checkstyle/releases/latest | grep browser_download_url | cut -d\" -f4)
 sed -i -E "s#URL=.*#URL='${URL}'#" bin/install-checkstyle.sh
 echo $URL | sed -E 's/.*checkstyle-(.*)-all.jar/\1/' > CHECKSTYLE_VERSION
