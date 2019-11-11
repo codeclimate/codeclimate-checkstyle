371ffafdd0f5130f46b8d065dcb43d592cb756e7
Filipe Esperandio <filipesperandio@gmail.com>
2019-11-11 18:30:31 -0300

    Upgrade to 8.26

--- bin/install-checkstyle.sh
+++ bin/install-checkstyle.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # use `make upgrade` to update this URL to the latest version
-URL='https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.25/checkstyle-8.25-all.jar'
+URL='https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.26/checkstyle-8.26-all.jar'
+
 
 wget -O /usr/local/bin/checkstyle.jar $URL
